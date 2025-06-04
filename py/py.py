9831593a8e8ccc2de08a1c66c631eb1687a4b143
benjaminn <buenardopolis10@gmail.com>
2025-06-04 19:04:10 -0300

    primera prueba

---
+++ py/py.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import sqlite3
+
+import sqlite3
+conexion = sqlite3.connect("producto.db")
+cursor = conexion.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS productos(id_producto INTEGER PRIMARY KEY AUTOINCREMENT, precio INTEGER,stock INTEGER,nombre TEXT)")
+cursor.execute("INSERT INTO productos (precio, stock,nombre) VALUES(?,?,?)", (33,33,"hsk"))
+conexion.commit()
+productos = cursor.execute("SELECT * FROM productos ")
+resultado =productos.fetchall()
+dicProductos = []
+for i in resultado:
+    dicProductos.append({"nombre": i[1], "precio": i[2],  "stock": i[3]})
+print(dicProductos)
+app = FastAPI()  
+@app.get("/")
+def main():
+    return dicProductos
