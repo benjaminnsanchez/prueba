730c3428b1d9e682ea7f0938a220abe5d0512481
benjaminn <buenardopolis10@gmail.com>
2025-06-06 22:06:02 -0300

    agregur middleware

--- py.py
+++ py.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 import sqlite3
 
-import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
+origins = ["https://prueba-4-6cgf.onrender.com","http://127.0.0.1:8000"]
+
 conexion = sqlite3.connect("producto.db")
 cursor = conexion.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS productos(id_producto INTEGER PRIMARY KEY AUTOINCREMENT, precio INTEGER,stock INTEGER,nombre TEXT)")
